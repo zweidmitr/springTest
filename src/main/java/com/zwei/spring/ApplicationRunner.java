58f6a01b34fa4a59c38153803139244749545b37
zweidmitr <zweidmitr@gmail.com>
2022-06-20 15:49:11 +0300

    lesson - 10 (lifecycle callbacks)

--- src/main/java/com/zwei/spring/ApplicationRunner.java
+++ src/main/java/com/zwei/spring/ApplicationRunner.java
@@ -7,11 +7,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var context = new ClassPathXmlApplicationContext("application.xml");
-        // clazz -> String -> Map<String,Object>
-        var connectionPool = context.getBean("p1", ConnectionPool.class);
-        System.out.println(connectionPool);
-        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-        System.out.println(companyRepository);
+        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
+            // clazz -> String -> Map<String,Object>
+            var connectionPool = context.getBean("p1", ConnectionPool.class);
+            System.out.println(connectionPool);
+            var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+            System.out.println(companyRepository);
+        }
     }
 }
