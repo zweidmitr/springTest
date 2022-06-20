d86f5864a03c5d9945d37c93245b07a23c43d13a
zweidmitr <zweidmitr@gmail.com>
2022-06-20 17:22:32 +0300

    lesson - 13 (custom beanFactoryPostProcessor)

---
+++ src/main/java/com/zwei/spring/bfpp/LogBeanFactoryPostProcessor.java
@@ -0,0 +1,19 @@
+package com.zwei.spring.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
+
+public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println();
+    }
+}
