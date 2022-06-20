07dd441ff4f1a671756afcb7edb25214530e9558
zweidmitr <zweidmitr@gmail.com>
2022-06-20 12:19:12 +0300

    lesson - 6 (constructor injection)

--- src/main/java/com/zwei/spring/ApplicationRunner.java
+++ src/main/java/com/zwei/spring/ApplicationRunner.java
@@ -8,7 +8,6 @@ public class ApplicationRunner {
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("application.xml");
         // clazz -> String -> Map<String,Object>
-
         var connectionPool = context.getBean("p1", ConnectionPool.class);
         System.out.println(connectionPool);
     }
