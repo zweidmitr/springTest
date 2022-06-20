cee1a12fe8f3662257e4967e0bea63920d483111
zweidmitr <zweidmitr@gmail.com>
2022-06-20 13:21:06 +0300

    lesson - 8 (property injection)

---
+++ src/main/java/com/zwei/spring/service/CompanyService.java
@@ -0,0 +1,9 @@
+package com.zwei.spring.service;
+
+public class CompanyService {
+    private UserService userService;
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+}
--- src/main/java/com/zwei/spring/service/UserService.java
+++ src/main/java/com/zwei/spring/service/UserService.java
@@ -6,9 +6,14 @@ import com.zwei.spring.database.repository.UserRepository;
 public class UserService {
     private final UserRepository userRepository;
     private final CompanyRepository companyRepository;
+    private CompanyService companyService;
 
     public UserService(UserRepository userRepository, CompanyRepository companyRepository) {
         this.userRepository = userRepository;
         this.companyRepository = companyRepository;
+    }
+
+    public void setCompanyService(CompanyService companyService) {
+        this.companyService = companyService;
     }
 }
