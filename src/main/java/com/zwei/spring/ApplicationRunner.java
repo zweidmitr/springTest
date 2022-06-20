81abd1a758d19e42655677b7ca4e0a9c49a5e151
zweidmitr <zweidmitr@gmail.com>
2022-06-20 11:48:18 +0300

    lesson - 5 (xml-based configuration)

--- src/main/java/com/zwei/spring/ApplicationRunner.java
+++ src/main/java/com/zwei/spring/ApplicationRunner.java
@@ -1,27 +1,15 @@
 package com.zwei.spring;
 
 import com.zwei.spring.database.pool.ConnectionPool;
-import com.zwei.spring.database.repository.CompanyRepository;
-import com.zwei.spring.database.repository.UserRepository;
-import com.zwei.spring.ioc.Container;
-import com.zwei.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var container = new Container();
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        // clazz -> String -> Map<String,Object>
 
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
-
-//        var connectionPool = container.get(ConnectionPool.class);
-//        var userRepository = container.get(UserRepository.class);
-//        var companyRepository = container.get(CompanyRepository.class);
-
-        var userService = container.get(UserService.class);
-
-
+        var connectionPool = context.getBean("p1", ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 }
