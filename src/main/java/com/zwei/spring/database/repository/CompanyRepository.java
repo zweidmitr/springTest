84a816b926a1a4b53889ec1804496182832eff6f
zweidmitr <zweidmitr@gmail.com>
2022-06-21 16:29:51 +0300

    lesson - 18 (@Autowired & @Value)

--- src/main/java/com/zwei/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/zwei/spring/database/repository/CompanyRepository.java
@@ -5,16 +5,26 @@ import com.zwei.spring.bpp.InjectBean;
 import com.zwei.spring.bpp.Transaction;
 import com.zwei.spring.database.entity.Company;
 import com.zwei.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.List;
 import java.util.Optional;
 
 @Transaction
 @Auditing
 public class CompanyRepository implements CrudRepository<Integer, Company> {
 
-    @InjectBean
-    private ConnectionPool connectionPool;
+    //    @Resource(name = "pool1")
+//    @Qualifier(value = "pool1")
+    private ConnectionPool pool1;
+    @Autowired
+    private List<ConnectionPool> pools;
+    @Value("${db.pool.size}")
+    private Integer poolSize;
 
     @PostConstruct
     private void init() {
@@ -31,7 +41,10 @@ public class CompanyRepository implements CrudRepository<Integer, Company> {
     @Override
     public void delete(Company entity) {
         System.out.println("delete method...");
+    }
 
-
+    @Autowired
+    public void setPool1(ConnectionPool pool1) {
+        this.pool1 = pool1;
     }
 }
