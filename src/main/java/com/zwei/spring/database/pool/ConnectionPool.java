13635b6eacad7b53dbcad50b515f3dd60d2a4c1f
zweidmitr <zweidmitr@gmail.com>
2022-06-21 12:30:19 +0300

    lesson - 14 (Annotation-based Configuration)

--- src/main/java/com/zwei/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/zwei/spring/database/pool/ConnectionPool.java
@@ -2,6 +2,8 @@ package com.zwei.spring.database.pool;
 
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Map;
 
@@ -25,6 +27,7 @@ public class ConnectionPool implements InitializingBean {
         this.properties = properties;
     }
 
+    @PostConstruct
     private void init() {
         System.out.println("Init connection pool");
     }
@@ -34,6 +37,7 @@ public class ConnectionPool implements InitializingBean {
         System.out.println("properties set");
     }
 
+    @PreDestroy
     private void destroy() {
         System.out.println("clean connection pool");
     }
