bee9fd779f9ea174baa66e04f3ac9b6f7e42c4ca
zweidmitr <zweidmitr@gmail.com>
2022-06-21 13:00:58 +0300

    lesson - 16 (custom beanPostProcessor.part1)

--- src/main/java/com/zwei/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/zwei/spring/database/repository/CompanyRepository.java
@@ -1,15 +1,10 @@
 package com.zwei.spring.database.repository;
 
+import com.zwei.spring.bpp.InjectBean;
 import com.zwei.spring.database.pool.ConnectionPool;
 
 public class CompanyRepository {
-    private final ConnectionPool connectionPool;
 
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 }
