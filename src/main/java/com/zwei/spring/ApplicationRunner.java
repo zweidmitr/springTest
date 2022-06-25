4f64d505ca47d1ea73862deaa6d1960aa83f42bc
zweidmitr <zweidmitr@gmail.com>
2022-06-25 15:42:41 +0300

    lesson - 24 (Java-based Configuration)

--- src/main/java/com/zwei/spring/ApplicationRunner.java
+++ src/main/java/com/zwei/spring/ApplicationRunner.java
@@ -1,9 +1,11 @@
 package com.zwei.spring;
 
+import com.zwei.spring.config.ApplicationConfiguration;
 import com.zwei.spring.database.pool.ConnectionPool;
 import com.zwei.spring.database.repository.CompanyRepository;
 import com.zwei.spring.database.repository.CrudRepository;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.Serializable;
@@ -16,7 +18,7 @@ public class ApplicationRunner {
         System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
         System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
 
-        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
+        try (var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
             // clazz -> String -> Map<String,Object>
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);
