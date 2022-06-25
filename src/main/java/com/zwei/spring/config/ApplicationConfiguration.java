6f4a26dade6071336eafaca512f7a55afe894e8f
zweidmitr <zweidmitr@gmail.com>
2022-06-25 16:16:29 +0300

    lesson - 26 (@Bean part1)

--- src/main/java/com/zwei/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/zwei/spring/config/ApplicationConfiguration.java
@@ -1,7 +1,11 @@
 package com.zwei.spring.config;
 
+import com.zwei.spring.database.pool.ConnectionPool;
 import com.zwei.spring.database.repository.CrudRepository;
+import com.zwei.spring.database.repository.UserRepository;
 import com.zwei.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Component;
@@ -18,4 +22,14 @@ import org.springframework.stereotype.Component;
                 @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
         })
 public class ApplicationConfiguration {
+    @Bean("pool2")
+    @Scope(BeanDefinition.SCOPE_PROTOTYPE)
+    public ConnectionPool pool2(@Value("${db.username}") String username) {
+        return new ConnectionPool(username, 20);
+    }
+
+    @Bean
+    public UserRepository userRepository2(ConnectionPool pool2) {
+        return new UserRepository(pool2);
+    }
 }
