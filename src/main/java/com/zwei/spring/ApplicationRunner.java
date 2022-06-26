94774190bc00f89170ca735aab959dcabebefe4c
zweidmitr <zweidmitr@gmail.com>
2022-06-26 14:25:59 +0300

    lesson - 33 (Spring Boot настройка проекта)

--- src/main/java/com/zwei/spring/ApplicationRunner.java
+++ src/main/java/com/zwei/spring/ApplicationRunner.java
@@ -1,37 +1,11 @@
 package com.zwei.spring;
 
-import com.zwei.spring.config.ApplicationConfiguration;
-import com.zwei.spring.database.pool.ConnectionPool;
-import com.zwei.spring.database.repository.CompanyRepository;
-import com.zwei.spring.database.repository.CrudRepository;
-import com.zwei.spring.service.CompanyService;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.io.Serializable;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        String value = "Hello";
-        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
-        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
-        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
-
-        try (var context = new AnnotationConfigApplicationContext()) {
-            context.register(ApplicationConfiguration.class);
-            context.getEnvironment().setActiveProfiles("web","prod");
-            /**
-             * refresh - reStart Beans
-             */
-            context.refresh();
-            // clazz -> String -> Map<String,Object>
-            var connectionPool = context.getBean("pool1", ConnectionPool.class);
-            System.out.println(connectionPool);
-
-            var companyService = context.getBean(CompanyService.class);
-            System.out.println(companyService.findById(1));
-        }
+        SpringApplication.run(ApplicationRunner.class, args);
     }
 }
