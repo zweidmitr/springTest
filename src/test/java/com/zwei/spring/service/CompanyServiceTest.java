198475419a086c1e1c1edc84643ae31a3cf12e7d
zweidmitr <zweidmitr@gmail.com>
2022-06-27 09:49:05 +0300

    lesson - 41 (Test Starter)

---
+++ src/test/java/com/zwei/spring/service/CompanyServiceTest.java
@@ -0,0 +1,49 @@
+package com.zwei.spring.service;
+
+import com.zwei.spring.database.entity.Company;
+import com.zwei.spring.database.repository.CrudRepository;
+import com.zwei.spring.dto.CompanyReadDto;
+import com.zwei.spring.listner.entity.EntityEvent;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CompanyServiceTest {
+
+    private static final Integer COMPANY_ID = 1;
+
+    @Mock
+    private CrudRepository<Integer, Company> companyRepository;
+    @Mock
+    private UserService userService;
+    @Mock
+    private ApplicationEventPublisher eventPublisher;
+    @InjectMocks
+    private CompanyService companyService;
+
+    @Test
+    void findById() {
+        doReturn(Optional.of(new Company(COMPANY_ID)))
+                .when(companyRepository).findById(COMPANY_ID);
+        var result = companyService.findById(COMPANY_ID);
+
+        assertTrue(result.isPresent());
+
+        var expectedResult = new CompanyReadDto(COMPANY_ID);
+        result.ifPresent(actual -> assertEquals(expectedResult, actual));
+
+        verify(eventPublisher).publishEvent(any(EntityEvent.class));
+        verifyNoMoreInteractions(eventPublisher, userService);
+    }
+}
