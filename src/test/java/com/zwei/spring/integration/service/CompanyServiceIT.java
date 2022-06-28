165fa78fc490b387edfc059503b140202694551d
zweidmitr <zweidmitr@gmail.com>
2022-06-28 20:07:17 +0300

    lesson - 43 (Integration Testing part3)

--- src/test/java/com/zwei/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/com/zwei/spring/integration/service/CompanyServiceIT.java
@@ -1,16 +1,21 @@
 package com.zwei.spring.integration.service;
 
 import com.zwei.spring.ApplicationRunner;
+import com.zwei.spring.config.DatabaseProperties;
 import com.zwei.spring.database.entity.Company;
 import com.zwei.spring.dto.CompanyReadDto;
+import com.zwei.spring.integration.annotation.IT;
 import com.zwei.spring.listner.entity.EntityEvent;
 import com.zwei.spring.service.CompanyService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
@@ -23,11 +28,12 @@ import static org.mockito.Mockito.*;
 //@ExtendWith(SpringExtension.class)
 //@ContextConfiguration(classes = ApplicationRunner.class,
 //initializers = ConfigDataApplicationContextInitializer.class)
-@SpringBootTest
+@IT
+@RequiredArgsConstructor
 public class CompanyServiceIT {
-    @Autowired
-    private CompanyService companyService;
     private static final Integer COMPANY_ID = 1;
+    private final CompanyService companyService;
+    private final DatabaseProperties databaseProperties;
 
     @Test
     void findById() {
@@ -37,5 +43,4 @@ public class CompanyServiceIT {
         var expectedResult = new CompanyReadDto(COMPANY_ID);
         result.ifPresent(actual -> assertEquals(expectedResult, actual));
     }
-
 }
