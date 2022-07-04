8da96471855b9f4f64957e42372db274e78b5bfd
zweidmitr <zweidmitr@gmail.com>
2022-07-04 10:09:03 +0300

    lesson - 54 (RepositoryQuery)

--- src/main/java/com/zwei/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/zwei/spring/database/repository/CompanyRepository.java
@@ -1,11 +1,12 @@
 package com.zwei.spring.database.repository;
 
 import com.zwei.spring.database.entity.Company;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
 
 import java.util.Optional;
 
-public interface CompanyRepository extends Repository<Company, Integer> {
+public interface CompanyRepository extends CrudRepository<Company, Integer> {
 
     public Optional<Company> findById(Integer id);
 
