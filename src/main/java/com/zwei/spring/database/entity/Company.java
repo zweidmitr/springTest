5c73fdd2b350903fe286ddcc6a1e2d326ed35ba0
zweidmitr <zweidmitr@gmail.com>
2022-07-07 10:22:08 +0300

    lesson - 56 (NamedQuery)

--- src/main/java/com/zwei/spring/database/entity/Company.java
+++ src/main/java/com/zwei/spring/database/entity/Company.java
@@ -9,6 +9,10 @@ import javax.persistence.*;
 import java.util.HashMap;
 import java.util.Map;
 
+@NamedQuery(
+        name= "Company.findByName",
+        query = "select c from Company c where lower (c.name) = lower(:name2)"
+)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
