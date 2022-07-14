83e6a0703b9e074ff4612ed591d5b2fcea8f31e1
zweidmitr <zweidmitr@gmail.com>
2022-07-14 09:16:00 +0300

    lesson - 61(@EntityGraph)

--- src/main/java/com/zwei/spring/database/entity/User.java
+++ src/main/java/com/zwei/spring/database/entity/User.java
@@ -7,6 +7,9 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+@NamedEntityGraph(
+        name = "User.company",
+        attributeNodes = @NamedAttributeNode("company"))
 @Data
 @ToString(exclude = "userChats")
 @EqualsAndHashCode(of = "username")
--- src/test/java/com/zwei/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/zwei/spring/integration/database/repository/UserRepositoryTest.java
@@ -28,11 +28,11 @@ class UserRepositoryTest {
         var pageable = PageRequest.of(0, 2, Sort.by("id"));
         var slice = userRepository.findAllBy(pageable);
 
-        slice.forEach(user -> System.out.println(user.getId()));
+        slice.forEach(user -> System.out.println(user.getCompany().getName()));
 
         while (slice.hasNext()) {
             slice = userRepository.findAllBy(slice.nextPageable());
-            slice.forEach(user -> System.out.println(user.getId()));
+            slice.forEach(user -> System.out.println(user.getCompany().getName()));
         }
 
     }
